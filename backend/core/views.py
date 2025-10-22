65ff43e79f72df7de9ee8796d43d67c05080599c
umarkhemis <umarkhemis9@gmail.com>
2025-10-22 10:06:45 +0300

    modified some codes

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,9 +1,32 @@
-from django.shortcuts import render
 
-from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 from .models import HotelInfo
 from .serializers import HotelInfoSerializer
 
-class HotelInfoDetail(generics.RetrieveAPIView):
-    queryset = HotelInfo.objects.all()
-    serializer_class = HotelInfoSerializer
+class HotelInfoDetail(APIView):
+    
+    def get(self, request):
+        hotel = HotelInfo.objects.first()
+        if not hotel:
+            return Response(
+                {"error": "Hotel information not found."},
+                status=status.HTTP_404_NOT_FOUND
+            )
+        
+        serializer = HotelInfoSerializer(hotel)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+
+
+
+
+
+
+
+
+
+
+
