4ee529712bdc5a4a0d8184f36bf27ce4b204a957
umarkhemis <umarkhemis9@gmail.com>
2025-10-30 20:15:56 +0300

    changed the views.py

--- backend/bookings/views.py
+++ backend/bookings/views.py
@@ -1,11 +1,5 @@
 
-<<<<<<< HEAD
-=======
 
-
-
-
->>>>>>> 910bc96 (booking fix 1)
 from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
